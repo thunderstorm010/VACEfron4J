89f2a27b1bcea94a37319b80d17ac6c4c4cba38c
thunderstorm010 <yigitozdem3@gmail.com>
2020-11-22 03:30:18 +0300

    Add dummy test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("maven")
+    id("maven-publish")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.github.thunderstorm010"
+version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     implementation("com.jsoniter:jsoniter:0.9.23")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
