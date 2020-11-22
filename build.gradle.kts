9bc691a46416864d7728dc4e0060ea033006b420
thunderstorm010 <66248615+thunderstorm010@users.noreply.github.com>
2020-11-22 03:35:36 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("maven-publish")
 }
 
 group = "com.github.thunderstorm010"
@@ -14,9 +13,4 @@ repositories {
 
 dependencies {
     implementation("com.jsoniter:jsoniter:0.9.23")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
